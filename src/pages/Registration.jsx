32e100840d5cd86a605143ea354c5d995de993e8
Yixbro <yixmanny9000@gmail.com>
2025-02-16 16:21:45 +0100

    tryna save it to repo

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 
 const Registration = () => {
+
+    const [companyName, setCompanyName] = useState('');
+    const [email, setEmail] = useState('');
+    const [tel, setTel] = useState('');
+
+    //url
+    const BASE_URL = 'https://multi-tenancy-chatbot-fastapi.onrender.com/register_company';
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        handleRequest();
+
+    }
+
+    const handleRequest = async () => {
+
+        try {
+            const request = await axios.post(BASE_URL, { name: companyName })
+            const data = request.data
+            console.log(data)
+        } catch (err) {
+            console.log(err);
+        }
+
+
+
+    }
+
     return (
         <div className='w-full'>
             <h4 className="text-3xl mt-3">Register your company</h4>
 
-            <form action="" className="flex flex-col space-y-5 mt-1">
+            <form action="" className="flex flex-col space-y-5 mt-1" onSubmit={handleSubmit}>
 
                 <div className="form-input-group">
                     <div className=''>
@@ -17,6 +46,7 @@ const Registration = () => {
                             className="form-control bg-dark text-white border-0"
                             placeholder=""
                             aria-describedby="helpId"
+                            onChange={(e) => setCompanyName(e.target.value)}
                         // style="--bs-focus-ring-color: rgba(var(--bs-success-rbg),.25)"
                         />
                     </div>
@@ -30,6 +60,7 @@ const Registration = () => {
                             className="form-control bg-dark text-white border-0"
                             placeholder=""
                             aria-describedby="helpId"
+                            onChange={(e) => setEmail(e.target.value)}
                         // style="--bs-focus-ring-color: rgba(var(--bs-success-rbg),.25)"
                         />
                     </div>
@@ -37,12 +68,12 @@ const Registration = () => {
                     <div>
                         <label htmlFor="companyname" className="form-label">Phone Number</label>
                         <input
-                            type="tel"
+                            type="number"
                             name="number"
                             id="number"
                             className="form-control bg-dark text-white border-0"
                             aria-describedby="helpId"
-                            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                            onChange={(e) => setTel(e.target.value)}
                         // style="--bs-focus-ring-color: rgba(var(--bs-success-rbg),.25)"
                         />
                     </div>
